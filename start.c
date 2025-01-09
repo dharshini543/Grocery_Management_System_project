d06b5173aa3f47efe1ca8513d81398b551cd851b
Dharshini T R <dharshinitr1998@gmail.com>
2025-01-09 13:07:22 +0530

    Updated

--- start.c
+++ start.c
@@ -70,7 +70,7 @@ int start()
         {
 
         case INVENTORY_MANAGEMENT:
-            printf("Enter\n1.Add Item to Inventory\n2.Delete Item from Inventory\n3.Update Inventory Item Details\n4.Display Inventory summary\n5.Sort Inventory By Name\n6.Sort Inventory By Department\n7.Sort Inventory By Price8.Sort Inventory By ID\n9.Get list by ID\n10.Display Deleted Items\n");
+            printf("Enter\n1.Add Item to Inventory\n2.Delete Item from Inventory\n3.Update Inventory Item Details\n4.Display Inventory summary\n5.Sort Inventory By Name\n6.Sort Inventory By Department\n7.Sort Inventory By Price\n8.Sort Inventory By ID\n9.Get list by ID\n10.Display Deleted Items\n");
             scanf("%d", &option);
 
             switch(option)
